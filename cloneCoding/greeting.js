03923c5ce6a940dd3848a927f1a87a5ad4915259
Common_fig <darjeeling4g@gmail.com>
2020-08-09 05:53:31 +0900

    day-6

--- cloneCoding/greeting.js
+++ cloneCoding/greeting.js
@@ -5,6 +5,22 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    const currentValue = input.value;
+    paingGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
+
 function paingGreeting(text) {
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -14,11 +30,14 @@ function paingGreeting(text) {
 function loadName() {
     const currentUser = localStorage.getItem(USER_LS);
     if (currentUser === null) {
+        askForName();
     } else {
         paingGreeting(currentUser);
     }
 }
 
-function init() {}
+function init() {
+    loadName();
+}
 
 init();
